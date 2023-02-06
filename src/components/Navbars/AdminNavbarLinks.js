ef58152695ed33f44bb91faedfa6d9e9ab43e31a
DESKTOP-5T0E1CO\AliAkbar <786aliakbar.aa@gmail.com>
2023-02-06 23:56:16 +0500

    Add Currency Update

--- src/components/Navbars/AdminNavbarLinks.js
+++ src/components/Navbars/AdminNavbarLinks.js
@@ -1,29 +1,33 @@
-// Chakra Icons
-import { BellIcon, SearchIcon } from "@chakra-ui/icons";
-// Chakra Imports
 import {
   Button,
   Flex,
-  IconButton,
-  Input,
-  InputGroup,
-  InputLeftElement,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuList,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
   useColorMode,
   Text,
   useColorModeValue,
+  useDisclosure,
+  FormControl,
+  FormLabel,
+  Select,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+  Radio,
+  HStack,
+  RadioGroup,
 } from "@chakra-ui/react";
-// Assets
-import avatar1 from "assets/img/avatars/avatar1.png";
-import avatar2 from "assets/img/avatars/avatar2.png";
-import avatar3 from "assets/img/avatars/avatar3.png";
+
 // Custom Icons
-import { ProfileIcon, SettingsIcon } from "components/Icons/Icons";
+import { ProfileIcon } from "components/Icons/Icons";
 // Custom Components
-import { ItemContent } from "components/Menu/ItemContent";
 import SidebarResponsive from "components/Sidebar/SidebarResponsive";
 import PropTypes from "prop-types";
 import React from "react";
@@ -31,14 +35,10 @@ import { NavLink } from "react-router-dom";
 import routes from "routes.js";
 
 export default function HeaderLinks(props) {
-  const { variant, children, fixed, secondary, onOpen, ...rest } = props;
+  const { variant, children, fixed, secondary, ...rest } = props;
   const { colorMode, toggleColorMode } = useColorMode();
-  // Chakra Color Mode
-  let mainTeal = useColorModeValue("teal.300", "teal.300");
-  let inputBg = useColorModeValue("white", "gray.800");
-  let mainText = useColorModeValue("gray.700", "gray.200");
+  const { isOpen, onOpen, onClose } = useDisclosure()
   let navbarIcon = useColorModeValue("gray.500", "gray.200");
-  let searchIcon = useColorModeValue("gray.700", "gray.200");
 
   if (secondary) {
     navbarIcon = "gray";
@@ -46,14 +46,15 @@ export default function HeaderLinks(props) {
   }
   const settingsRef = React.useRef();
   return (
+    <>
     <Flex
       pe={{ sm: "0px", md: "16px" }}
       w={{ sm: "100%", md: "auto" }}
       alignItems="center"
       flexDirection="row"
     >
-      <Button style={{marginRight:'1rem'}}>
-      <Text display={{ sm: "none", md: "flex" }}>Update Currency</Text>
+      <Button onClick={onOpen} style={{marginRight:'1rem'}}>
+      <Text  display={{ sm: "none", md: "flex" }}>Update Currency</Text>
       </Button>
       <NavLink to="/auth/signin">
         <Button
@@ -91,6 +92,58 @@ export default function HeaderLinks(props) {
         {...rest}
       />
     </Flex>
+    <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Update Currency</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <FormControl isRequired style={{'marginBottom': '1rem'}}>
+              <FormLabel>Currency</FormLabel>
+              <Select placeholder='Select Currency'>
+                <option value="USD">USD</option>
+                <option value="PKR">PKR</option>
+                <option value="AUD">AUD</option>
+              </Select>
+            </FormControl>
+            <FormControl isRequired style={{'marginBottom': '1rem'}}>
+              <FormLabel>Amount</FormLabel>
+              <NumberInput max={50} min={10}>
+                <NumberInputField/>
+                <NumberInputStepper>
+                  <NumberIncrementStepper/>
+                  <NumberDecrementStepper/>
+                </NumberInputStepper>
+              </NumberInput>
+            </FormControl>
+            <FormControl isRequired style={{'marginBottom': '1rem'}}>
+              <FormLabel>Currency Rate</FormLabel>
+              <NumberInput max={50} min={10}>
+                <NumberInputField/>
+                <NumberInputStepper>
+                  <NumberIncrementStepper/>
+                  <NumberDecrementStepper/>
+                </NumberInputStepper>
+              </NumberInput>
+            </FormControl>
+            <FormControl isRequired as='fieldset'>
+              <RadioGroup>
+                <HStack spacing='24px'>
+                  <Radio value='cask'>Cash</Radio>
+                  <Radio value='pending'>Pending</Radio>
+                </HStack>
+              </RadioGroup>
+            </FormControl>
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3}>
+              Update
+            </Button>
+            <Button variant='ghost' onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
 
@@ -98,5 +151,5 @@ HeaderLinks.propTypes = {
   variant: PropTypes.string,
   fixed: PropTypes.bool,
   secondary: PropTypes.bool,
-  onOpen: PropTypes.func,
+  // onOpen: PropTypes.func,
 };
